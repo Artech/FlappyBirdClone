ab258043b94a330b613c01b4f0f37780bfbaf006
Sara Z <sara.z@tamprojects.com>
2015-04-11 16:02:02 +0300

    Add prototype for movement

---
+++ Assets/GameAssets/Scripts/Prototype/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float FlapSpeed = 200.0f;
+	// Verticalinput must be float because GetAxisRaw returns float
+	public float VerticalInput;
+
+	private Rigidbody2D RB2D;
+
+
+	// Use this for initialization
+	void Start () {
+		// Get the RigidBody component
+		// Before Unity 5 we use to use rigidboy2D instance 
+		//but in Unity 5 they removed it so we have to get the component first
+		RB2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// GetAxisRaw returns a direction either -1, 0, or 1 
+		VerticalInput = Input.GetAxisRaw("Vertical") * FlapSpeed;
+
+	}
+
+	void FixedUpdate(){
+
+		if(VerticalInput > 0)
+		{
+			// Activate Gravity 
+			// Move up with each press. Can't hold and press to fly to infinity and beyond of the screen. 
+			// Double Jump 
+			// Clamp the jump
+			RB2D.gravityScale = 1;
+			RB2D.AddRelativeForce(new Vector2(0.0f, VerticalInput));
+		}
+
+	}
+}
---
+++ Assets/GameAssets/Scripts/TestScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Awake ()
+	{
+		Debug.Log("Am awake");
+	}
+
+	void Start()
+	{
+		Debug.Log("Am starting");
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.Log( "Update realTime: "+Time.deltaTime);
+	
+	}
+
+	void FixedUpdate()
+	{
+		Debug.Log( "FixedUpdate realTime: "+Time.deltaTime);
+	}
+}
